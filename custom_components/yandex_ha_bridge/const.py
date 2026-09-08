1c54940d0e3113f8354584aa7d4cb686bc173533
menfis911 <94356322+menfis911@users.noreply.github.com>
2026-09-08 16:26:21 +0300

    fix: sync integration version to 0.3.3

--- custom_components/yandex_ha_bridge/const.py
+++ custom_components/yandex_ha_bridge/const.py
@@ -3,7 +3,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "yandex_ha_bridge"
 NAME = "Yandex HA Bridge"
-VERSION = "0.3.2"
+VERSION = "0.3.3"
 CONF_TOKEN = "token"
 CONF_DEVICE_IDS = "device_ids"
 DEFAULT_SCAN_INTERVAL = 30
