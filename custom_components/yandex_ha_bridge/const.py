3b46150e56b95a53fc63a939add3bd287205c120
menfis911 <94356322+menfis911@users.noreply.github.com>
2026-09-08 16:01:52 +0300

    fix: use unique Yandex HA Bridge domain

---
+++ custom_components/yandex_ha_bridge/const.py
@@ -0,0 +1,10 @@
+"""Constants."""
+from homeassistant.const import Platform
+
+DOMAIN = "yandex_ha_bridge"
+NAME = "Yandex HA Bridge"
+VERSION = "0.3.1"
+CONF_TOKEN = "token"
+CONF_DEVICE_IDS = "device_ids"
+DEFAULT_SCAN_INTERVAL = 30
+PLATFORMS = [Platform.LIGHT]
