fc0c4ec87fad36ced5206d2ca5acb6a377b069ed
menfis911 <94356322+menfis911@users.noreply.github.com>
2026-09-09 17:59:47 +0300

    chore: bump const version to 0.6.0

--- custom_components/yandex_ha_bridge/const.py
+++ custom_components/yandex_ha_bridge/const.py
@@ -2,7 +2,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "yandex_ha_bridge"
 NAME = "Yandex HA Bridge"
-VERSION = "0.5.1"
+VERSION = "0.6.0"
 CONF_TOKEN = "token"
 CONF_DEVICE_IDS = "device_ids"
 DEFAULT_SCAN_INTERVAL = 30
