e92d9dc29b33041bd3a7ab704c3b82f8b8fb575a
menfis911 <94356322+menfis911@users.noreply.github.com>
2026-09-09 17:06:38 +0300

    bump integration version to 0.4.1

--- custom_components/yandex_ha_bridge/const.py
+++ custom_components/yandex_ha_bridge/const.py
@@ -2,7 +2,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "yandex_ha_bridge"
 NAME = "Yandex HA Bridge"
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 CONF_TOKEN = "token"
 CONF_DEVICE_IDS = "device_ids"
 DEFAULT_SCAN_INTERVAL = 30
